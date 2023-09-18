4735c10820ded94330049c2ab78ada0ba715f4ad
dukim <sajacall82@naver.com>
2023-09-18 11:26:36 +0900

    feat: create print_hello function
    num loop - 안녕 출력

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-for i in range(5):
-    print(f'{i} - hello')
+def print_hello(num: int):
+    """
+    print hello
+    :param num: loop count
+    :return: None
+    """
+    for i in range(num):
+        print(f'{i}번째 안녕')
+
+if __name__=='__main__':
+    print_hello(10)
