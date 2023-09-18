b21e4920b63848c37e371acd2a26706baaff8117
dukim <sajacall82@naver.com>
2023-09-18 12:50:05 +0900

    feat: 15의 배수일 경우 fizzbuzz 출력
    if num is 15's multiple, print fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,7 +6,9 @@ def fizzbuzz(num: int):
 
     """
     for n in range(1, num+1):
-        if n%3 == 0:
+        if n%15 == 0:
+            print('fizzbuzz')
+        elif n%3 == 0:
             print('fizz')
         elif n%5 == 0:
             print('buzz')
