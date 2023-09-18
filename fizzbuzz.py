ca0efd360bedcdacf77284045da59e8c60743c38
dukim <sajacall82@naver.com>
2023-09-18 12:13:27 +0900

    feat: print buzz
    if num is 5'multiple, print buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,12 @@
-print('hello')
+def fizzbuzz(num):
+    """
+    if num is 5's multiple, print buzz
+    """
+    for n in range(num):
+        if n%5 == 0:
+            print('buzz')
+        else: print(n)
+
+
+if __name__ == '__main__':
+    fizzbuzz(15)
