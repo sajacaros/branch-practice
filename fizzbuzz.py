7a0a11635033dd41d2e07e8cd00e933bdb96fd52
dukim <sajacall82@naver.com>
2023-09-18 12:10:58 +0900

    feat: print fizz
    if num is 3'multipl, print fizz
    otherwize print num

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,15 @@
-print('hello')
+def fizzbuzz(num: int):
+    """
+    if num is 3's multiple, print fizz,
+    otherwise print num
+
+    """
+    for n in range(num):
+        if n%3 == 0:
+            print('fizz')
+        else: print(n)
+
+
+
+if __name__ == '__main__':
+    fizzbuzz(15)
